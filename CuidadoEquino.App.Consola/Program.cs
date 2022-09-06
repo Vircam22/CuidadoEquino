45fce0b84ba46866b7ed9a83fea36bb514e2b607
Diego Santana Leal <dsantanaleal@gmail.com>
2022-09-05 23:28:56 -0500

    Adding example to add new Caballo

--- CuidadoEquino.App.Consola/Program.cs
+++ CuidadoEquino.App.Consola/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CuidadoEquino.App.Dominio;
+using CuidadoEquino.App.Persistencia;
 
 namespace CuidadoEquino.App.Consola
 {
@@ -8,6 +9,19 @@ namespace CuidadoEquino.App.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            using(var context = new Persistencia.AppContext())
+            {
+                var caballo = new Caballo()
+                {
+                    Nombre = "Rocky",
+                    Color = "Gris",
+                    Especie = "Pony",
+                    Raza = "Manso"
+                };
+                context.Caballos.Add(caballo);
+                context.SaveChanges();
+            }
         }
     }
 }
